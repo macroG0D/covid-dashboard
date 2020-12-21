f2d9a9f0517c9dffbb3b4280ee7f117ce1045630
Diana Martyn <dianamartin790@gmail.com>
2020-12-22 00:28:27 +0200

    feat: responsive chart

--- src/js/modules/Graph.js
+++ src/js/modules/Graph.js
@@ -1,64 +1,57 @@
+import CurrentCountry from './CurrentCountry';
 import DataFetcher from './DataFetcher';
 
 const Chart = require('chart.js');
 
-async function fillArrayWithPerMonthCases(dataFetcher, country) {
-  const result = [];
-  for (let i = 8; i < 12; i += 1) {
-    // eslint-disable-next-line no-await-in-loop
-    // eslint-disable-next-line max-len
-    // const response = await dataFetcher.fetchDataByCountryAndDate(country, `2020-${i < 10 ? `0${i}` : i}-01T00:00:00Z`, `2020-${i + 1 < 10 ? `0${i + 1}` : i + 1}-01T00:00:00Z`);
-    // const mappedToCases = response.map((el) => el.Cases);
-    // const monthSum = mappedToCases[mappedToCases.length - 1] - mappedToCases[0];
-    // result.push(monthSum);
-  }
-  return result;
-}
-
 export default class Graph {
   static chart = Chart;
 
-  static showChart(currentCountry) {
+  static showChart() {
     const ctx = document.getElementById('myChart').getContext('2d');
-    // const dataFetcher = new DataFetcher(); // TODO DYNAMIC
-    const graphData = fillArrayWithPerMonthCases(DataFetcher, currentCountry);
+
+    const currentCountryTimeline = DataFetcher.data[CurrentCountry.selectedCountryID].timeline;
+    const labelsData = Object.keys(currentCountryTimeline.cases);
+    const casesData = Object.values(currentCountryTimeline.cases);
+    const deathCasesData = Object.values(currentCountryTimeline.deaths);
+    const recoveredCasesData = Object.values(currentCountryTimeline.recovered);
 
     const myChart = new Chart(ctx, {
       type: 'line',
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
       data: {
-        labels: ['August', 'September', 'October', 'November'],
+        labels: labelsData,
         datasets: [
           {
-            label: `${currentCountry}`,
+            label: 'cases',
             fill: false,
 
-            data: graphData,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-            ],
+            data: casesData,
+            backgroundColor: 'rgba(0, 0, 255)',
+            borderColor: 'rgb(0,0,255)',
+            borderWidth: 1,
+          },
+          {
+            label: 'recovered',
+            fill: false,
+
+            data: recoveredCasesData,
+            backgroundColor: 'rgba(0, 255, 0)',
+            borderColor: 'green',
+            borderWidth: 1,
+          },
+          {
+            label: 'death',
+            fill: false,
+
+            data: deathCasesData,
+            backgroundColor: 'rgba(255, 0, 0)',
+            borderColor: 'red',
             borderWidth: 1,
           },
         ],
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
       },
     });
   }
